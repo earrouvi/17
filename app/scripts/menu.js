488553dad2003f10fb43328a095dcd45e77c23b8
Elsa Arrou-Vignod <earrou-vignod@lucca.fr>
2014-10-30 17:39:20 +0100

    Fix : meilleure gestion des photos aléatoires : on évite les mêmes motifs sur la même page

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -2,27 +2,42 @@
 
 angular.module('controllers')
 	.controller('MenuCtrl', ['$scope', '$http', '$routeParams', '$location', function ($scope, $http, $routeParams, $location) {
-		var randArray = [];
 
 		$scope.activeLink = function(linkName) {
 			return $location.path() == '/' + linkName;
 		};
 
-		$scope.goToPage = function(page) {
-			$location.path(page);
-			randArray = [];
+		$scope.displaySubPage = function(linkName) {
+			$scope.showSub[linkName] = true;
 		};
 
-		$scope.randomNb = function() {
-			var rand = Math.floor(Math.random() * 17) + 1;
-			/*if (_.contains(randArray, rand)) {
-				rand = (rand + 1) % 17;
-			}*/
-			randArray.push(rand);
-			return rand;
+		$scope.goToPage = function(page) {
+			$location.path(page);
+			$scope.init();
+		};
+
+		$scope.randomNb = function(index) {
+			return $scope.randArray[index];
+		};
+
+		$scope.generateRandomPhotos = function() {
+			for (var i = 0; i < 4; i++) {
+				var rand = Math.floor(Math.random() * 17) + 1;
+				while (_.contains($scope.randArray, rand)) {
+					rand = (rand + 1) % 17;
+				}
+				$scope.randArray.push(rand);
+			}
 		};
 
 		$scope.init = function() {
+			$scope.showSub = {};
+			$scope.showSub['jeudi'] = false;
+			$scope.showSub['vendredi'] = false;
+			$scope.showSub['weekend'] = false;
+
+			$scope.randArray = [];
+			$scope.generateRandomPhotos();
 		};
 		$scope.init();
 	}]);
