b852969eafd9bf05920f5c18de083a09e193ac8a
Elsa Arrou-Vignod <earrou-vignod@lucca.fr>
2014-10-30 16:42:53 +0100

    Logique pour les photos aléatoires

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -2,12 +2,24 @@
 
 angular.module('controllers')
 	.controller('MenuCtrl', ['$scope', '$http', '$routeParams', '$location', function ($scope, $http, $routeParams, $location) {
+		var randArray = [];
+
 		$scope.activeLink = function(linkName) {
 			return $location.path() == '/' + linkName;
 		};
 
 		$scope.goToPage = function(page) {
 			$location.path(page);
+			randArray = [];
+		};
+
+		$scope.randomNb = function() {
+			var rand = Math.floor(Math.random() * 17) + 1;
+			/*if (_.contains(randArray, rand)) {
+				rand = (rand + 1) % 17;
+			}*/
+			randArray.push(rand);
+			return rand;
 		};
 
 		$scope.init = function() {
