a8bea302c02cfe348154e99eb0e08e4d0e7efe1e
Elsa Arrou-Vignod <earrou-vignod@lucca.fr>
2014-11-24 14:22:31 +0100

    Horloge parlante et modif de certains textes

--- app/scripts/what.js
+++ app/scripts/what.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('controllers')
-	.controller('WhatCtrl', ['$scope', '$http', '$routeParams', '$location', function ($scope, $http, $routeParams, $location) {
+	.controller('WhatCtrl', ['$scope', '$http', '$routeParams', '$location', '$window', function ($scope, $http, $routeParams, $location, $window) {
 		$scope.minutes = function() {
 			$scope.ca_part = Math.floor(moment('08/05/2015', 'DD/MM/YYYY').diff(moment(), 'minutes'));
+		};
+
+		$scope.goToHorlogeParlante = function() {
+			$window.location.href = 'http://www.horlogeparlante.com/';
 		};
 
 		$scope.displaySubPage = function(linkName) {
